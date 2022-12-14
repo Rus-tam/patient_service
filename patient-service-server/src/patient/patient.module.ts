e3566351f5d2c5b90e74c910e08b8a6fddc1ae30
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-12-14 18:07:13 +0500

    connection to DB

--- patient-service-server/src/patient/patient.module.ts
+++ patient-service-server/src/patient/patient.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PatientService } from './patient.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PatientEntity } from './entities/patient.entity';
+import { MedicalExaminationEntity } from './entities/medicalExamination.entity';
+import { TomographyEntity } from './entities/tomography.entity';
 
 @Module({
-  providers: [PatientService]
+  imports: [
+    TypeOrmModule.forFeature([
+      PatientEntity,
+      MedicalExaminationEntity,
+      TomographyEntity,
+    ]),
+    ConfigModule,
+  ],
+  providers: [PatientService],
+  exports: [PatientService],
 })
 export class PatientModule {}
