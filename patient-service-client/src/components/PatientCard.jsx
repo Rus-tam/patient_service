53fa486c7e21809558ca021f30510824a86e4c0b
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-12-25 20:22:30 +0500

    some changes-5

--- patient-service-client/src/components/PatientCard.jsx
+++ patient-service-client/src/components/PatientCard.jsx
@@ -2,21 +2,54 @@ import React, { useEffect, useState } from "react";
 import NavBar from "./layout/Navbar";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const PatientCard = () => {
   const param = useParams();
   const id = param.id;
-  const [patientData, setPatientData] = useState();
+  const [patientData, setPatientData] = useState({
+    name: "",
+    surname: "",
+    patronymic: "",
+    patientBirthDate: "",
+    phone: "",
+    medicalExaminations: [],
+    tomography: [],
+  });
   useEffect(() => {
     axios.get(`http://localhost:5000/patient/${id}`).then((resp) => {
       setPatientData(resp.data);
     });
   }, [setPatientData]);
 
+  console.log(patientData.name);
+
   return (
     <div>
       <NavBar />
-      <h1>Hello</h1>
+
+      <h2 className="mb-3 pt-3">Карта пациента</h2>
+      <hr />
+      <p>
+        <strong>Имя пациента: </strong>
+        {patientData.name}
+      </p>
+      <p>
+        <strong>Фимилия пациента: </strong>
+        {patientData.surname}
+      </p>
+      <p>
+        <strong>Отчество пациента: </strong>
+        {patientData.patronymic}
+      </p>
+      <p>
+        <strong>Дата рождения пациента: </strong> {patientData.patientBirthDate}
+      </p>
+      <p>
+        <strong>Телефон пациента: </strong>
+        {patientData.phone}
+      </p>
     </div>
   );
 };
