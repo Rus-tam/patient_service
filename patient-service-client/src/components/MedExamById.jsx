c6dd3b7ff7755b8fa33b27b63a8d22cb0eb5f96c
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-12-26 16:13:20 +0500

    redirect button

--- patient-service-client/src/components/MedExamById.jsx
+++ patient-service-client/src/components/MedExamById.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "./layout/Navbar";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import moment from "moment";
+import Button from "react-bootstrap/Button";
 
 const MedExamById = () => {
+  const navigate = useNavigate();
   const { medExamId } = useParams();
 
   const [medExam, setMedExam] = useState({
@@ -30,9 +32,9 @@ const MedExamById = () => {
       <NavBar />
       <h2 className="mb-3 pt-3">Результаты осмотра</h2>
       <hr />
-      <h3>
+      <h2 className="pb-4">
         {medExam.patient.surname} {medExam.patient.name} {medExam.patient.patronymic}
-      </h3>
+      </h2>
       <p>
         <strong>Дата осмотра: </strong> {moment(medExam.createdAt).format("YYYY-MM-DD")}
       </p>
@@ -51,6 +53,17 @@ const MedExamById = () => {
       <p>
         <strong>Результаты осмотра: </strong> <br /> {medExam.examinationResult}
       </p>
+
+      <Button
+        onClick={() =>
+          navigate({
+            pathname: `/patient-card/${medExam.patient.id}`,
+          })
+        }
+        variant="primary"
+      >
+        Назад
+      </Button>
     </div>
   );
 };
