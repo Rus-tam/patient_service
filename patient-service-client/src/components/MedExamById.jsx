dfafea5abbdaedd68a20174983a89151bda74a7d
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-01-13 17:58:20 +0500

    changes in MedExamById component

--- patient-service-client/src/components/MedExamById.jsx
+++ patient-service-client/src/components/MedExamById.jsx
@@ -13,7 +13,22 @@ const MedExamById = () => {
   const [medExam, setMedExam] = useState({
     createdAt: "",
     AMDType: "",
-    visualAcuity: "",
+    visualAcuityOD: "",
+    visualAcuityOS: "",
+    tonometryOD: "",
+    tonometryOS: "",
+    refractometryODsph: "",
+    refractometryODcyl: "",
+    refractometryODax: "",
+    refractometryOSsph: "",
+    refractometryOScyl: "",
+    refractometryOSax: "",
+    biomicroscopyOD: "",
+    biomicroscopyOS: "",
+    eyeBottomOD: "",
+    eyeBottomOS: "",
+    additionalExamOD: "",
+    additionalExamOS: "",
     nextInjectionDate: "",
     nextInspectionDate: "",
     examinationResult: "",
@@ -22,16 +37,16 @@ const MedExamById = () => {
     VEGFTherapyHistory: "",
   });
 
-  const [nextInjectionDate, setnextInjectionDate] = useState();
+  const [nextInjectionDate, setNextInjectionDate] = useState();
   const [nextInspectionDate, setNextInspectionDate] = useState();
 
   useEffect(() => {
     axios.get(`http://localhost:5000/patient/med-exam/${medExamId}`).then((resp) => {
       setMedExam(resp.data);
-      setnextInjectionDate(resp.data.nextInjectionDate);
+      setNextInjectionDate(resp.data.nextInjectionDate);
       setNextInspectionDate(resp.data.nextInspectionDate);
     });
-  }, [setMedExam, setnextInjectionDate, setNextInspectionDate]);
+  }, [setMedExam, setNextInjectionDate, setNextInspectionDate]);
 
   const updateMedExam = async (update) => {
     const resp = await axios.put(`http://localhost:5000/patient/med-exam/${medExamId}/update`, update);
@@ -49,19 +64,49 @@ const MedExamById = () => {
 
     if (medExam.AMDType === "wet") {
       const update = {
-        visualAcuity: e.target[0].value,
-        nextInjectionDate: e.target[1].value,
-        nextInspectionDate: e.target[2].value,
-        examinationResult: e.target[3].value,
+        visualAcuityOD: e.target[0].value,
+        visualAcuityOS: e.target[1].value,
+        tonometryOD: e.target[2].value,
+        tonometryOS: e.target[3].value,
+        refractometryODsph: e.target[4].value,
+        refractometryODcyl: e.target[5].value,
+        refractometryODax: e.target[6].value,
+        refractometryOSsph: e.target[7].value,
+        refractometryOScyl: e.target[8].value,
+        refractometryOSax: e.target[9].value,
+        biomicroscopyOD: e.target[10].value,
+        biomicroscopyOS: e.target[11].value,
+        eyeBottomOD: e.target[12].value,
+        eyeBottomOS: e.target[13].value,
+        additionalExamOD: e.target[14].value,
+        additionalExamOS: e.target[15].value,
+        nextInjectionDate: e.target[16].value,
+        nextInspectionDate: e.target[17].value,
+        examinationResult: e.target[18].value,
       };
 
       await updateMedExam(update);
     } else {
       const update = {
-        visualAcuity: e.target[0].value,
-        nextInspectionDate: e.target[1].value,
-        examinationResult: e.target[2].value,
-        VEGFTherapyHistory: e.target[3].value,
+        visualAcuityOD: e.target[0].value,
+        visualAcuityOS: e.target[1].value,
+        tonometryOD: e.target[2].value,
+        tonometryOS: e.target[3].value,
+        refractometryODsph: e.target[4].value,
+        refractometryODcyl: e.target[5].value,
+        refractometryODax: e.target[6].value,
+        refractometryOSsph: e.target[7].value,
+        refractometryOScyl: e.target[8].value,
+        refractometryOSax: e.target[9].value,
+        biomicroscopyOD: e.target[10].value,
+        biomicroscopyOS: e.target[11].value,
+        eyeBottomOD: e.target[12].value,
+        eyeBottomOS: e.target[13].value,
+        additionalExamOD: e.target[14].value,
+        additionalExamOS: e.target[15].value,
+        nextInspectionDate: e.target[16].value,
+        examinationResult: e.target[18].value,
+        VEGFTherapyHistory: e.target[19].value,
       };
 
       await updateMedExam(update);
@@ -69,7 +114,7 @@ const MedExamById = () => {
   };
 
   const setInjection = (e) => {
-    setnextInjectionDate(e.target.value);
+    setNextInjectionDate(e.target.value);
   };
 
   const setInspection = (e) => {
@@ -90,15 +135,77 @@ const MedExamById = () => {
 
       <Form onSubmit={saveChanges}>
         <Form.Group className="mb-3" controlId="visualAcuity">
-          <Form.Label>
+          <p>
             <strong>Острота зрения: </strong>
-          </Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={2}
-            placeholder="Введите данные по остроте зрения"
-            defaultValue={medExam.visualAcuity}
-          />
+          </p>
+          <Form.Label>OD</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OD" defaultValue={medExam.visualAcuityOD} />
+          <Form.Label>OS</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OS" defaultValue={medExam.visualAcuityOS} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="tonometry">
+          <p>
+            <strong>Тонометрия: </strong>
+          </p>
+          <Form.Label>OD</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OD, мм.рт.ст." defaultValue={medExam.tonometryOD} />
+          <Form.Label>OS</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OS, мм.рт.ст." defaultValue={medExam.tonometryOS} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="refractometry">
+          <p>
+            <strong>Рефрактометрия: </strong>
+          </p>
+          <p>
+            <strong>OD: </strong>
+          </p>
+          <Form.Label>sph</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OD, sph" defaultValue={medExam.refractometryODsph} />
+          <Form.Label>cyl</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OD, cyl" defaultValue={medExam.refractometryODcyl} />
+          <Form.Label>ax</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OD, ax" defaultValue={medExam.refractometryODax} />
+          <p className="mt-2">
+            <strong>OS: </strong>
+          </p>
+          <Form.Label>sph</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OS, sph" defaultValue={medExam.refractometryOSsph} />
+          <Form.Label>cyl</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OS, cyl" defaultValue={medExam.refractometryOScyl} />
+          <Form.Label>ax</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OS, ax" defaultValue={medExam.refractometryOSax} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="biomicroscopy">
+          <p>
+            <strong>Биомикроскопия: </strong>
+          </p>
+          <Form.Label>OD</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OD" defaultValue={medExam.biomicroscopyOD} />
+          <Form.Label>OS</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OS" defaultValue={medExam.biomicroscopyOS} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="eyeBottom">
+          <p>
+            <strong>Глазное дно: </strong>
+          </p>
+          <Form.Label>OD</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OD" defaultValue={medExam.eyeBottomOD} />
+          <Form.Label>OS</Form.Label>
+          <Form.Control as="textarea" rows={1} placeholder="OS" defaultValue={medExam.eyeBottomOS} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="additionalExamination">
+          <p>
+            <strong>Дополнительные исследования: </strong>
+          </p>
+          <Form.Label>OD</Form.Label>
+          <Form.Control as="textarea" rows={2} placeholder="OD" defaultValue={medExam.additionalExamOD} />
+          <Form.Label>OS</Form.Label>
+          <Form.Control as="textarea" rows={2} placeholder="OS" defaultValue={medExam.additionalExamOS} />
         </Form.Group>
 
         {medExam.AMDType === "wet" && (
@@ -129,18 +236,6 @@ const MedExamById = () => {
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="medicalexam">
-          <Form.Label>
-            <strong>Результаты осмотра: </strong>
-          </Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={4}
-            placeholder="Введите результаты осмотра"
-            defaultValue={medExam.examinationResult}
-          />
-        </Form.Group>
-
         {medExam.AMDType === "dry" && (
           <div>
             <p>
@@ -156,6 +251,18 @@ const MedExamById = () => {
                 rows={4}
                 placeholder="Введите результаты"
                 defaultValue={medExam.VEGFTherapyHistory}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="medicalexam">
+              <Form.Label>
+                <strong>Результаты осмотра: </strong>
+              </Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={4}
+                placeholder="Введите результаты осмотра"
+                defaultValue={medExam.examinationResult}
               />
             </Form.Group>
           </div>
