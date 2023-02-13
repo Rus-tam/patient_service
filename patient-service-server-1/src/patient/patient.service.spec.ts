14621c0b605a63bf08ede5e54926a937e853a1bd
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-02-13 16:50:49 +0500

    patient service testing

--- patient-service-server-1/src/patient/patient.service.spec.ts
+++ patient-service-server-1/src/patient/patient.service.spec.ts
@@ -1,18 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PatientService } from './patient.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Connection, Repository } from "typeorm";
+import { AppModule } from "../app.module";
+import { PatientEntity } from "./entities/patient.entity";
+import { MedicalExaminationEntity } from "./entities/medicalExamination.entity";
+import { TomographyEntity } from "./entities/tomography.entity";
+import * as dotenv from "dotenv";
 
-describe('PatientService', () => {
-  let service: PatientService;
+dotenv.config();
 
-  beforeEach(async () => {
+describe("PostgreSQL", () => {
+  let app: INestApplication;
+  let connection: Connection;
+  let patientRepository: Repository<PatientEntity>;
+  let medicalExaminationRepository: Repository<MedicalExaminationEntity>;
+  let tomographyRepository: Repository<TomographyEntity>;
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PatientService],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: "postgres",
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DATABASE,
+          entities: [PatientEntity, MedicalExaminationEntity, TomographyEntity],
+          synchronize: true,
+        }),
+        AppModule,
+      ],
     }).compile();
 
-    service = module.get<PatientService>(PatientService);
+    app = module.createNestApplication();
+    await app.init();
+
+    connection = module.get<Connection>(Connection);
+    patientRepository = connection.getRepository(PatientEntity);
+    medicalExaminationRepository = connection.getRepository(MedicalExaminationEntity);
+    tomographyRepository = connection.getRepository(TomographyEntity);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterAll(async () => {
+    await connection.close();
+    await app.close();
   });
+
+  it("should be defined", () => {
+    expect(patientRepository).toBeDefined();
+  }, 60);
 });
